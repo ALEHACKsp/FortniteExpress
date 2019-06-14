41635dc053cd92978377d90af191dd377d4f4e82
Jason Charnley <jason.charnley@LDNML4TXJH6.clients.global.arup.com>
2019-06-14 16:49:58 +0100

    fixed express routes so no 404

--- routes/history.js
+++ routes/history.js
@@ -2,26 +2,30 @@ var express = require('express');
 var router = express.Router();
 const Request = require("request");
 
-router.get('/', function (req, res, error) {
-    var accountID = req.query.accountID;
-    var options = {
-      url: `https://api.fortnitetracker.com/v1/profile/account/${accountID}/matches`,
+router.get('/', function (req, res) {
+    const accountid = "954f2029-7c09-41b5-ac03-a9aad2c94809";
+    const options = {
+      url: `https://api.fortnitetracker.com/v1/profile/account/${accountid}/matches`,
       method: 'GET',
       headers: {
         "TRN-Api-Key": "f92be6be-cb17-43c7-96e5-6a442ab5b65e"
       }
     };
-     if(error) {
-      res.status(404).send("AccountId not found");
-     } else  {
       Request.get(options, (error, response,  body) => {
         if (error) {
-         res.status(404).send("Request failed")
+            return console.dir(error);
         }
-         const data = JSON.parse(body);
-         res.status(200).send(data)
+         try {
+             const data = JSON.parse(body);
+             res.status(200).send(data)
+         }
+         catch {
+            res.status(500).send("Error with the request")
+         }
      });
-     }
   });
 
   module.exports = router;
+
+
+  
